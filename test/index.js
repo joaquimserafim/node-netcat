659c06910d90fd8d42afe909e6bf8052c863aa10
joaquimserafim <joaquim.serafim@gmail.com>
2014-10-12 14:43:51 +0100

    code coverage in green :)

--- test/index.js
+++ test/index.js
@@ -1,16 +1,43 @@
 var test = require('tape');
 var Netcat = require('../');
 
+var server;
+var client;
 
-test('server & client', function(t) {
+test('nc server constructor - dont pass any param', function(assert) {
+  assert.plan(1);
+
+  try {
+    server = Netcat.server();
+  } catch(ex) {
+    assert.ok(ex, ex);
+  }
+});
+
+test('nc server constructor - pass only port & options', function(assert) {
+  assert.plan(3);
+  server = Netcat.server(4000, {
+    readEncoding: 'utf8',
+    timeout: 3000
+  });
+  assert.ok(server);
+  server.listen();
+  server.once('ready', function() { 
+    assert.pass('server, ready'); 
+    server.close(function() {
+      assert.pass('server closed');  
+    });
+  });  
+});
+
+test('server & client using binary data', function(t) {
   t.plan(13);
  
-  var server = Netcat.server(4000);
-  var client = Netcat.client(4000, { readEncoding: 'utf8' });
+  server = Netcat.server(4000);
+  client = Netcat.client(4000);
 
   server.once('ready', function() { 
     t.pass('server, ready');
-
     client.start();
   });
 
@@ -21,7 +48,8 @@ test('server & client', function(t) {
 
     // test Buffer
     t.equal(Buffer.isBuffer(data),
-      true, 'server, is configure to rx as Buffer');
+      true,
+      'server, is configure to rx as Buffer');
 
     var clients = server.getClients();
     t.ok(clients, 'server, exists ' + clients.length + ' client active');
@@ -44,9 +72,76 @@ test('server & client', function(t) {
   });
 
   server.once('error', function(err) { t.error(err !== null, err); });
-
   server.once('close', function() { t.pass('server, closed'); });
+  server.listen();
 
+
+  // client
+  client.once('open', function() { 
+    t.pass('client, connected'); 
+    setTimeout(function () {
+      client.send('Hello World', function() {
+        t.pass('client, send message');
+      });
+    }, 1000);
+  });
+
+  client.on('data', function(data) {
+    t.equal(data.length > -1, true, 'client, receive data: ' + data);
+    // test encoding
+    t.equal(Buffer.isBuffer(data),
+      true,
+      'client, is configure to rx binary data');
+  });
+
+  client.once('close', function() {
+    t.pass('client, closed');
+  });
+});
+
+test('server & client using utf8 encoding', function(t) {
+  t.plan(13);
+ 
+  server = Netcat.server(4000, {readEncoding: 'utf8'});
+  client = Netcat.client(4000, {readEncoding: 'utf8'});
+
+  server.once('ready', function() { 
+    t.pass('server, ready');
+    client.start();
+  });
+
+  server.on('data', function(client, data) {
+    t.equal(data.length > 0, 
+      true,
+      'server, receive data: "' + data + '" from client ' + client);
+
+    // test Buffer
+    t.equal(typeof data,
+      'string',
+      'server, is configure to rx string data');
+
+    var clients = server.getClients();
+    t.ok(clients, 'server, exists ' + clients.length + ' client active');
+
+    Object.keys(clients).forEach(function(client) {
+      server.send(clients[client], data, true, function() {
+        t.pass('server, send "' + data + '" to client ' + clients[client]);
+      });
+    });
+
+    setTimeout(function() { server.close(); }, 1000);
+  });
+
+  server.on('client_on', function(client) { 
+    t.ok(client, 'server, client connect ' + client); 
+  });
+
+  server.on('client_off', function(client) { 
+    t.ok(client, 'server, client disconnet ' + client); 
+  });
+
+  server.once('error', function(err) { t.error(err !== null, err); });
+  server.once('close', function() { t.pass('server, closed'); });
   server.listen();
 
 
@@ -64,7 +159,8 @@ test('server & client', function(t) {
     t.equal(data.length > -1, true, 'client, receive data: ' + data);
     // test encoding
     t.equal(typeof data,
-      'string', 'client, is configure to rx as String ascii/utf8');
+      'string',
+      'client, is configure to rx string data');
   });
 
   client.once('close', function() {
@@ -90,8 +186,8 @@ test('portscan', function(t) {
 test('upd', function(t) {
   t.plan(5);
 
-  var server = Netcat.udpServer(5000, '127.0.0.1');
-  var client = Netcat.udpClient(5000, '127.0.0.1');
+  server = Netcat.udpServer(5000, '127.0.0.1');
+  client = Netcat.udpClient(5000, '127.0.0.1');
 
   server.on('data', function(msg, client, protocol) {
     t.ok(msg,
@@ -109,16 +205,12 @@ test('upd', function(t) {
   });
   
   server.once('error', function(err) { t.error(err !== null, err); });
-
   server.once('close', function() { t.pass('server, closed'); });
-
   server.bind();
 
 
   client.on('open', function() { t.pass('client, open'); });
-
   client.once('error', function(err) { t.error(err !== null, err); });
-
   client.once('close', function() { t.pass('client, closed'); });
 });
 
